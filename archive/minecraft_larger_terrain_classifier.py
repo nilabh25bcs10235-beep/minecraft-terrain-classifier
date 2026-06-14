f16c24469c92d80f93972cb56ad2fb538367a2e6
nilabh25bcs10235-beep <nilabh.25bcs10235@sst.scaler.com>
2026-06-14 22:19:54 +0530

    Delete archive/minecraft_larger_terrain_classifier.py

--- archive/minecraft_larger_terrain_classifier.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""
-Minecraft Larger + Less Dropout Terrain Classifier
-
-This was the best performing hyperparameter configuration before the final balanced v2.
-"""
-
-print("Archived Larger + Less Dropout version.")
